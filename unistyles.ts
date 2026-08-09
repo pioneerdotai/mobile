3b3eac7c35ed8f236314f9c73fc3c6b271ecf00c
alexanderoskin <alexander.oskin@gmail.com>
2026-08-09 13:33:22 +0300

    Add neutral color 150 to palette

--- unistyles.ts
+++ unistyles.ts
@@ -173,6 +173,7 @@ const colors = {
     neutral: {
         50: '#fafafa',
         100: '#f5f5f5',
+        150: '#eeeeee',
         200: '#e5e5e5',
         300: '#d4d4d4',
         400: '#a3a3a3',
