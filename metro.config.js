b4426b6c3e6b1ae96b1172e53ec69f4db419e271
alexanderoskin <alexander.oskin@gmail.com>
2026-08-24 14:45:52 +0300

    Update Metro patches and config for v0.84.5
    Rename metro and metro-runtime patches from v0.84.4 to v0.84.5, update
    patchedDependencies, remove custom worklets resolver in favor of
    getBundleModeMetroConfig, and add --clear-cache to build commands

--- metro.config.js
+++ metro.config.js
@@ -1,28 +1,10 @@
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
-const path = require('path');
 const { getBundleModeMetroConfig } = require('react-native-worklets/bundleMode');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 let config = getSentryExpoConfig(__dirname);
 
 config.resolver.sourceExts.push('sql');
-
-config.watchFolders.push(path.resolve(__dirname, 'node_modules/react-native-worklets/.worklets'));
-
-const defaultResolver = config.resolver.resolveRequest;
 config = getBundleModeMetroConfig(config);
-const bundleModeResolver = config.resolver.resolveRequest;
-
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-    if (moduleName.startsWith('react-native-worklets/.worklets/')) {
-        return bundleModeResolver(context, moduleName, platform);
-    }
-
-    if (defaultResolver) {
-        return defaultResolver(context, moduleName, platform);
-    }
-
-    return context.resolveRequest(context, moduleName, platform);
-};
 
 module.exports = config;
