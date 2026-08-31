c13cb4e21a0b63868f63e144741592048b293074
alexanderoskin <alexander.oskin@gmail.com>
2026-08-31 14:35:16 +0300

    Remove unused icon style from CloseButton

--- src/components/buttons/close.tsx
+++ src/components/buttons/close.tsx
@@ -1,18 +1,11 @@
 import { X } from 'lucide-react-native';
-import { StyleSheet } from 'react-native-unistyles';
 
 import { BaseIconButton, type BaseIconButtonProps } from './base-icon-button';
 
 export type CloseButtonProps = Omit<BaseIconButtonProps, 'Icon' | 'iconStyle'>;
 
-const styles = StyleSheet.create((theme) => ({
-    icon: {
-        marginLeft: -theme.space(0.5),
-    },
-}));
-
 const CloseButton = (props: CloseButtonProps) => {
-    return <BaseIconButton {...props} Icon={X} iconStyle={styles.icon} />;
+    return <BaseIconButton {...props} Icon={X} />;
 };
 
 export { CloseButton };
